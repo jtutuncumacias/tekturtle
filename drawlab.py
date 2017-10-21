da61a012819fd15953dd83e1559b3ad35aeb5d3c
Julian Tutuncu-Macias <jtutuncu@Euclid.wv.cc.cmu.edu>
2017-10-21 16:49:19 -0400

    create drawlab
    drawlab will have students use loops to draw regular polygons, eventually leading to drawing a circle

---
+++ drawlab.py
@@ -0,0 +1,17 @@
+import turtle
+
+# Basics of moving turtle, pen up/down
+
+
+#The turtle Trixie will draw a triangle:
+
+Trixie = turtle.Turtle()
+turtle.color("blue")
+turtle.shape("turtle")
+
+for num in range (0, 2):
+    Trixie.forward(60)
+    Trixie.left(60)
+
+#Now, you will have the turtle  draw a square:
+
