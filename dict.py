a43d8744bc2c175505d699a7f0ae29b588f223e1
claire wang <basketballclaire@gmail.com>
2017-10-22 14:31:23 -0400

    gridlines, code ordering

--- dict.py
+++ dict.py
@@ -2,35 +2,60 @@ import turtle
 
 #Dictionaries visualization lab (using turtle)
 
-#   TODO:
-#   add gridlines
+
+#----------STARTER CODE BEGINS----------#
+
+grid = turtle.Turtle()
+grid.color("gray")
+grid.hideturtle()
+grid.speed(0)
+for num in range(-10, 11):
+    grid.penup()
+    grid.goto(-200, num * 20)
+    grid.pendown()
+    grid.goto(200, num * 20)
+for num in range(-10, 11):
+    grid.penup()
+    grid.goto(num * 20, -200)
+    grid.pendown()
+    grid.goto(num * 20, 200)
+
+bob = turtle.Turtle()       # our main character bob
+bob.shape("turtle")
 
 sally = turtle.Turtle()
 sally.shape("turtle")
 sally.color("blue")
-sally.setpos(100, 0)
+sally.penup()
+sally.goto(100, 0)        #sally lives at the coordinates (100, 0)
 
 alex = turtle.Turtle()
 alex.shape("turtle")
 alex.color("green")
-alex.setpos(160, 80)
+alex.penup()
+alex.goto(160, 80)        #alex lives at (160, 80)
+
+#----------STARTER CODE ENDS----------#
+
+
+# Exercise 1: get bob to where sally lives
 
 addressbook = {'sally': 5, 'carly': 2}
 
-bob = turtle.Turtle()
-bob.shape("turtle")
 for num in range(addressbook['sally']):    #bob moves forward the number of steps it takes to get to sally
     bob.forward(20)
 
-# with lists (turtle goes in 2+ directions)
+# Exercise 2: with lists (turtle goes in 2+ directions)
+# get bob to where alex lives
 
-bob.setpos(0, 0)
+bob.goto(0, 0)    #bob goes back to his original position
 
 alex_address = [8, 4]
 dave_address = [2, 2]
 
 addressbook2 = {'alex': alex_address,
                 'dave': dave_address}
+
 
 for num in range((addressbook2['alex'])[0]):
     bob.forward(20)
