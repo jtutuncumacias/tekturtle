0301eb416e26494b46202e3da2fa032ff3d6fabc
Dmcdominic <dmcdominic@gmail.com>
2017-11-10 12:29:04 -0500

    Edits to Turtle Lab 1. Added Started Code and Sample End Result

---
+++ Turtle Lab 1 - Functions/Sample End Result.py
@@ -0,0 +1,60 @@
+#!/bin/python3
+import turtle
+
+# You can tell Python to ignore a line of code by putting a "#" at the start
+# After completing each challenge, starting at number 2, do this to all your code
+
+
+# Challenge 1:
+turtle.Screen().bgcolor("white")
+turtle.color("blue")
+turtle.shape("turtle")
+
+
+# Challenge 2:
+#turtle.forward(250)
+#turtle.backward(500)
+#turtle.forward(250)
+
+
+# Challenge 3:
+#turtle.forward(100)
+#turtle.right(90)
+#turtle.forward(100)
+#turtle.right(90)
+#turtle.forward(100)
+#turtle.right(90)
+#turtle.forward(100)
+#turtle.right(90)
+
+#turtle.forward(100)
+#turtle.backward(200)
+#turtle.forward(100)
+#turtle.right(90)
+#turtle.forward(100)
+#turtle.backward(200)
+#turtle.forward(100)
+
+
+# Challenge 4:
+#turtle.goto(100, 0)
+#turtle.goto(100, 100)
+#turtle.goto(0, 100)
+#turtle.goto(0, 0)
+#turtle.goto(0, 100)
+#turtle.goto(50, 150)
+#turtle.goto(100, 100)
+
+
+# Challenge 5:
+turtle.forward(50)
+turtle.penup()
+turtle.goto(0, 50)
+turtle.left(135)
+turtle.stamp()
+turtle.goto(50, 50)
+turtle.right(90)
+turtle.stamp()
+turtle.goto(25, 100)
+turtle.write("I like turtles.")
+turtle.goto(200, 100)
---
+++ Turtle Lab 1 - Functions/Starter Code.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+import turtle
+
+# You can tell Python to ignore a line of code by putting a "#" at the start
+# Once you finish each challenge, starting at number 2, do this to all of your code
+
+
+# Challenge 1:
+turtle.Screen().bgcolor("black")
+turtle.color("white")
+turtle.shape("turtle")
+
+
+# Challenge 2:
+
+
+
+# Challenge 3:
+
+
+
+# Challenge 4:
+
+
+
+# Challenge 5:
+
+
